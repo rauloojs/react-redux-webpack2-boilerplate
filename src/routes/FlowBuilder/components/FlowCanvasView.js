b3aa78b3b3e95860d3b3a4de67e59ae5bf138578
Raúl Ojeda <rauloojs@gmail.com>
2017-05-30 18:36:14 -0500

    Adding draggable elements

--- src/routes/FlowBuilder/components/FlowCanvasView.js
+++ src/routes/FlowBuilder/components/FlowCanvasView.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import FlowItems from './FlowItems';
 import Box from 'grommet/components/Box';
+import JsPlumb from '../../../modules/JsPlumb'
 
 
 export default class FlowCanvasView extends Component {
+  componentDidMount() {
+    let canvas = document.getElementById('canvas');
+    console.log(canvas);
+    // JsPlumb.setContainer();
+  }
   render() {
     let nodes = this.props.nodes;
 
     return (
       <div className='flow-canvas-view'>
-        <div className='canvas'>
+        <div className='canvas' id='canvas'>
           <FlowItems flowItems={nodes} />
         </div>
       </div>
