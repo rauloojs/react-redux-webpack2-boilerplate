b9bb41fe4e9c224b9e968d82a7b0d057a3b39cdb
Raúl Ojeda <rauloojs@gmail.com>
2017-05-30 18:05:25 -0500

    Adding jsplumb to project

--- src/routes/FlowBuilder/components/FlowItems.js
+++ src/routes/FlowBuilder/components/FlowItems.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import FlowItem from './FlowItem';
+import JsPlumb from '../../../modules/JsPlumb'
 
 
 export default class FlowItems extends Component {
   componentDidMount() {
-      this.makeFlowItemsDraggable();
+    this.makeFlowItemsDraggable();
   }
   makeFlowItemsDraggable() {
-
+    JsPlumb.ready(() => {
+      console.log(document.querySelectorAll('.flow-item'));
+      JsPlumb.draggable(document.querySelectorAll('.flow-item'), {
+        stop: function (e) {
+          console.log(e.pos[0], e.pos[1]);
+          // component.props.dispatch(updateCanvasItemPosition(component.props.item.id, e.pos[0], e.pos[1]))
+        },
+        containment: true
+      });
+    });
   }
   render() {
     let flowItems = this.props.flowItems;
